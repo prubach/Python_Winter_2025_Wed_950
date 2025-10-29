f5883106780fd99d7ba25287c7b868fe26faaaa2
prubac <prubac@sgh.waw.pl>
2025-10-29 10:58:02 +0100

    strings

--- string_vars.py
+++ string_vars.py
@@ -32,3 +32,17 @@ print(f'o_L={o_L}')
 
 c = chr(97)
 print(f'c={c}')
+
+print('------------------')
+
+
+print('------------------')
+s_m2 = r'hello \t\tworld \nit\'s Wednesday \n29th \t\tOctober'
+s_m2 = 'hello \t\tworld \nit\'s Wednesday \\\n29th \October'
+print(s_m2)
+
+ll_i = s_m2.find('lll')
+#ll_i = s_m2.index('lll')
+print(f'll_i={ll_i}')
+lines = s_m2.split('\n')
+print(f'lines={lines}')
