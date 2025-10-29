272891f6897c5c4ea1758dc5bb427faca75cd816
prubac <prubac@sgh.waw.pl>
2025-10-29 12:43:44 +0100

    Loops

---
+++ loops.py
@@ -0,0 +1,21 @@
+for i in range(2, 10, 2): # start at 2 and go until 10 with a step of 2
+    #print(i)
+    print(i, end='\t')
+print('\n--------')
+for i in reversed(range(10)):
+    print(i, end='\t')
+
+print('\n--------')
+li = [33, 67, 47 ]
+for el in li:
+    print(el, end='\t')
+
+print('\n--------')
+
+j = 0
+summ = 0
+while j < len(li):
+    summ = summ + li[j]
+    print(summ, end='\t')
+    j = j + 1
+#print(summ)
