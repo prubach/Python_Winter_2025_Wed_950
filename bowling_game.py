07e24d544555d6f06558955e303f19609f9cd364
prubac <prubac@sgh.waw.pl>
2025-11-12 10:04:22 +0100

    Bowling game exercise

---
+++ bowling_game.py
@@ -0,0 +1,19 @@
+"""
+* * * * * *
+ * * * * *
+  * * * *
+   * * *
+    * *
+     *
+
+n - number of rows of bowls
+
+Bowling
+
+How many bowls do we need given the "n"
+"""
+
+def sum_bowls(n):
+    sum_b = 0
+    # ....
+    return sum_b
