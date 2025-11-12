b03ca503ca002c0321e95169a19d798466aed445
prubac <prubac@sgh.waw.pl>
2025-11-12 10:40:07 +0100

    Bowling game - solutions

--- bowling_game.py
+++ bowling_game.py
@@ -13,7 +13,27 @@ Bowling
 How many bowls do we need given the "n"
 """
 
-def sum_bowls(n):
+def sum_bowls_recurs(n):
+    if n == 1:
+        return 1
+    else:
+        prev_n = sum_bowls_recurs(n - 1)
+        return prev_n + n
+
+
+def sum_bowls_loop(n):
     sum_b = 0
-    # ....
+    for i in range(1, n + 1):
+        sum_b += i
     return sum_b
+
+
+def sum_bowls_seq(n):
+    return round((n*(n+1))/2)
+
+
+n = 5
+print(f'sum using sum_bowls_loop,    n={n}: {sum_bowls_loop(n)}')
+print(f'sum using sum_bowls_seq,     n={n}: {sum_bowls_seq(n)}')
+print(f'sum using sum_bowls_recurs,  n={n}: {sum_bowls_recurs(n)}')
+
