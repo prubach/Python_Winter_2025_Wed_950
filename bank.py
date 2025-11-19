5a9c5dcef6d27ff75969cd44e832aa5b9710075a
prubac <prubac@sgh.waw.pl>
2025-11-19 13:15:11 +0100

    Bank - exceptions

--- bank.py
+++ bank.py
@@ -17,6 +17,20 @@ class Account:
         self.id = Account.last_id
         self._balance = 0
 
+    def deposit(self, amount):
+        if amount <= 0:
+            print('Invalid amount.')
+            raise InvalidAmountException(f'Invalid amount: {amount}')
+        else:
+            self._balance += amount
+
+    def charge(self, amount):
+        if amount > self._balance:
+            print('Insufficient funds.')
+            raise InsufficientFundsException(f'Insufficient funds: {amount}')
+        else:
+            self._balance -= amount
+
     def __repr__(self):
         return f'Acc[{self.id}, {self.customer.last_name}, {self._balance}]'
 
@@ -39,11 +53,37 @@ class Bank:
     def __repr__(self):
         return f'Bank[{self.name}: \n{self.customer_list}\n{self.account_list}]'
 
+class BankException(Exception):
+    pass
+
+class InsufficientFundsException(BankException):
+    pass
+
+class InvalidAmountException(BankException):
+    pass
+
+
+if __name__ == '__main__':
+    bank = Bank('')
+
 
 bank = Bank('SGH Bank')
 
 c1 = bank.create_customer('John', 'Smith')
 a1 = bank.create_account(c1)
+
+try:
+    #a = 500
+    #s = 'avcv' + a
+    a1.deposit(-500)
+    a1.charge(300)
+except InsufficientFundsException as ife:
+    print(ife)
+except InvalidAmountException as iae:
+    print(iae)
+# except Exception as e:
+#     print(e)
+
 a2 = bank.create_account(c1)
 print(bank)
 c2 = bank.create_customer('Anne', 'Brown')
